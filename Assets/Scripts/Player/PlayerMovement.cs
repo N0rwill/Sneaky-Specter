3c9bcae980800fa4f1d281bb30430a71838a8f49
Norwill <willeverett206@gmail.com>
2025-02-19 15:20:23 -0500

    Crouch
    Added player Crouch that make the player 6-% the original size and slows them down by half.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,8 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    private float originalMoveSpeed;
 
     public float groundDrag;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    [Header("KeyBinds")]
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -26,6 +35,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        // get the original scale of the player
+        startYScale = transform.localScale.y;
+        // get the original speed of the player
+        originalMoveSpeed = moveSpeed;
     }
 
     void Update()
@@ -52,6 +66,24 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        // start crouch
+        if (Input.GetKeyDown(crouchKey))
+        {
+            // set player scale to crouch scale
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            // add force to push player down
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+            moveSpeed = crouchSpeed;
+        }
+
+        // end crouch
+        if (Input.GetKeyUp(crouchKey))
+        {
+            // set player scale back to normal
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            moveSpeed = originalMoveSpeed;
+        }
     }
 
     // move the player
