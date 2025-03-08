b7906c5c45fb35da3570b4ed9180eb6fe77eb5ab
averiefiedler <avefied@gmail.com>
2025-03-07 22:17:51 -0500

    UI STUFF!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public void StartGame()
+    {
+        SceneManager.LoadScene("EarlyLevel");
+        Debug.Log("Game has loaded.");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start");
+        Debug.Log("Returned to Menu.");
+    }
+
+    public void Lose()
+    {
+        SceneManager.LoadScene("Lose");
+        Debug.Log("Lose has loaded.");
+    }
+
+    public void Win()
+    {
+        SceneManager.LoadScene("Win");
+        Debug.Log("Win has loaded.");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Application has quit.");
+    }
     // Start is called before the first frame update
     void Start()
     {
