87e8771b4262ecfac54a4c0a4500df8a1c5c3de3
Norwill <willeverett206@gmail.com>
2025-03-11 19:41:39 -0400

    Book UI works!

---
+++ Assets/Scripts/Interactables/ReadBook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadBook : MonoBehaviour
+{
+    public GameObject bookUI;
+
+    private bool isOpen;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            bookUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void OpenBook()
+    {
+        if (!isOpen)
+        {
+            bookUI.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        if (isOpen)
+        {
+            bookUI.SetActive(false);
+        }
+    }
+}
