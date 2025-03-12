54846c0df533f9f53f14aa5e8ad7d54aabd0402b
averiefiedler <avefied@gmail.com>
2025-03-12 14:52:03 -0400

    sfx

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
